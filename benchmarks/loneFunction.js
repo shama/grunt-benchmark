b116827aaad1d091146949530774f91177f34424
Larry Davis <lazd@lazd.net>
2013-04-23 21:21:19 -0700

    Sample tests for Benchmark.js

--- benchmarks/loneFunction.js
+++ benchmarks/loneFunction.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(done) {
-  setTimeout(done, 100);
+  setTimeout(done, 5);
 };
---
+++ benchmarks/singleTest.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  name: 'Timeout Test',
+  fn: function(done) {
+    setTimeout(done, 5);
+  }
+}
