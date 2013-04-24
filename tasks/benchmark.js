0d2e80068befb5222af18f2ffd17aef06b941700
Larry Davis <lazd@lazd.net>
2013-04-24 05:42:42 -0700

    Fix bug where suite's tests disappeared when ran twice, report performance difference in number of times between fastest and second fastest test(s), better log output for tied tests

--- tasks/benchmark.js
+++ tasks/benchmark.js
@@ -53,7 +53,7 @@ module.exports = function(grunt) {
     var tests;
     var runnable;
     var benchmarkInfo = require(path.join(process.cwd(), src));
-
+    
     if (typeof benchmarkInfo === 'function') {
       /*
         // A lone function named by its file
@@ -63,73 +63,78 @@ module.exports = function(grunt) {
       benchmarkOptions.fn = benchmarkInfo;
       singleBenchmark = true;
     }
-    else if (typeof benchmarkInfo.name === 'string' && typeof benchmarkInfo.fn === 'function') {
-      if (benchmarkInfo.tests) {
-        grunt.log.error('Invalid benchmark: "'+benchmarkOptions.name+'" specify either export.fn or export.tests ');
-        return next();
-      }
-      /*
-        // A single test
-        module.exports = {
-          name: String,  // Test name
-          fn: Function, // Test function
-          [setup: Function],  // Other Benchmark parameters
-          [teardown: Function] // etc
-      */
-      benchmarkOptions = benchmarkInfo;
-      singleBenchmark = true;
-    }
-    else if (benchmarkInfo.tests) {
-      /*
-        // A suite of tests
-        module.exports = {
-          name: String, // Suite name
-          tests: Object, // Object keyed on test name
-          [setup: Function],  // Other Benchmark parameters
-          [teardown: Function] // etc
+    else {
+      // Copy it so we can modify it without breaking future tests
+      benchmarkInfo = grunt.util._.extend({}, benchmarkInfo);
+      
+      if (typeof benchmarkInfo.name === 'string' && typeof benchmarkInfo.fn === 'function') {
+        if (benchmarkInfo.tests) {
+          grunt.log.error('Invalid benchmark: "'+benchmarkOptions.name+'" specify either export.fn or export.tests ');
+          return next();
         }
-      */
-      
-      // Set name
-      benchmarkInfo.name = benchmarkInfo.name || path.basename(src, '.js');
-      
-      // Extract tests
-      tests = benchmarkInfo.tests;
-      delete benchmarkInfo.tests;
-      
-      // Add in options
-      grunt.util._.extend(benchmarkOptions, benchmarkInfo);
-      
-      if (Array.isArray(tests)) {
-        // Ensure all tests are test objects with valid names
-        tests = tests.map(function(obj, index) {
-          obj = objectify(obj);
-          
-          // Explicitly give a name or the output of Benchmark.js' filter('winner') command is an empty string
-          if (!obj.name) {
-            obj.name = '<Test #'+(index+1)+'>';
+        /*
+          // A single test
+          module.exports = {
+            name: String,  // Test name
+            fn: Function, // Test function
+            [setup: Function],  // Other Benchmark parameters
+            [teardown: Function] // etc
+        */
+        benchmarkOptions = benchmarkInfo;
+        singleBenchmark = true;
+      }
+      else if (benchmarkInfo.tests) {
+        /*
+          // A suite of tests
+          module.exports = {
+            name: String, // Suite name
+            tests: Object, // Object keyed on test name
+            [setup: Function],  // Other Benchmark parameters
+            [teardown: Function] // etc
           }
+        */
+      
+        // Set name
+        benchmarkInfo.name = benchmarkInfo.name || path.basename(src, '.js');
+      
+        // Extract tests
+        tests = benchmarkInfo.tests;
+        delete benchmarkInfo.tests;
+      
+        // Add in options
+        grunt.util._.extend(benchmarkOptions, benchmarkInfo);
+      
+        if (Array.isArray(tests)) {
+          // Ensure all tests are test objects with valid names
+          tests = tests.map(function(obj, index) {
+            obj = objectify(obj);
           
-          return obj;
-        });
+            // Explicitly give a name or the output of Benchmark.js' filter('winner') command is an empty string
+            if (!obj.name) {
+              obj.name = '<Test #'+(index+1)+'>';
+            }
+          
+            return obj;
+          });
+        }
+        else {
+          // Convert tests to an array of test objects
+          tests = grunt.util._.map(tests, function(obj, key) {
+            obj = objectify(obj);
+          
+            // name can be specified as the key or as a property of the test object
+            if (!obj.name) {
+              obj.name = key;
+            }
+          
+            return obj;
+          });
+        }
       }
       else {
-        // Convert tests to an array of test objects
-        tests = grunt.util._.map(tests, function(obj, key) {
-          obj = objectify(obj);
-          
-          // name can be specified as the key or as a property of the test object
-          if (!obj.name) {
-            obj.name = key;
-          }
-          
-          return obj;
-        });
+        grunt.log.error('Invalid configuration: '+src+' does not contain a valid test object or test suite', benchmarkInfo);
+        return next();
       }
-    }
-    else {
-      grunt.log.error('Invalid configuration: "'+benchmarkOptions.name+'" missing is incorrect');
-      return next();
     }
     
     if (singleBenchmark) {
@@ -168,7 +173,62 @@ module.exports = function(grunt) {
       
       runnable.on('complete', function() {
         if (!this.error) {
-          grunt.log.writeln('Fastest is ' + Benchmark.pluck(Benchmark.filter(this, 'fastest'), 'name'));
+          // Get the tests
+          var tests = grunt.util._.sortBy(this, 'hz');
+          
+          // Get the top fastest tests
+          var fastestTests = Benchmark.filter(this, 'fastest');
+          
+          // Only bother if more than one test
+          if (tests.length <= 1) return;
+          
+          // Get the testest test
+          var fastest = fastestTests[0];
+          
+          // Extract their names
+          var fastestNames = Benchmark.pluck(fastestTests, 'name');
+          
+          // Get the second fastest
+          var secondFastestTests;
+          var secondFastest;
+          var secondFastestNames;
+          if (fastestTests.length > 1) {
+            secondFastestTests = Benchmark.filter(fastestTests.slice(1), 'fastest');
+            secondFastest = secondFastestTests[0];
+            secondFastestNames = Benchmark.pluck(secondFastestTests, 'name');
+          }
+          else {
+            var slowerTests = grunt.util._.reject(tests, function(obj) {
+              return ~fastestNames.indexOf(obj.name);
+            });
+            secondFastestTests = Benchmark.filter(slowerTests, 'fastest').reverse();
+            secondFastest = secondFastestTests[0];
+            secondFastestNames = Benchmark.pluck(secondFastestTests, 'name');
+          }
+          
+          // Calculate how much faster the fastest functions were than the second fastest
+          var timesFaster = (fastest.hz/secondFastest.hz).toFixed(1);
+          
+          var isAre = 'test is';
+          if (fastestTests.length > 1)
+            isAre = 'tests are';
+          
+          var message = 'Fastest ' + isAre + ' ' + grunt.log.wordlist([fastestNames], { separator: ' and ' });
+          
+          // Give increases color based on order of magnitude of increase
+          var increaseColor = false;
+          if (timesFaster > 1.5)
+            increaseColor = 'green';
+          if (timesFaster > 10)
+            increaseColor = 'yellow';
+          if (timesFaster > 100)
+            increaseColor = 'red';
+          
+          // Only bother if there wasn't a tie
+          if (fastestTests.length !== tests.length)
+            message += ' at ' + grunt.log.wordlist([Benchmark.formatNumber(timesFaster)+'x'], { color: increaseColor }) + ' faster than ' + grunt.log.wordlist(secondFastestNames, { separator: ' and '});
+          
+          grunt.log.writeln(message);
         }
       });
     }
@@ -176,7 +236,7 @@ module.exports = function(grunt) {
     // Add listeners
     runnable.on('error', function(event) {
       var target = event.target;
-      grunt.log.error('Error running test "'+target.name+'": '+target.error);
+      grunt.log.error('Error running test '+target.name+': '+target.error);
     });
     
     runnable.on('complete', function() {
